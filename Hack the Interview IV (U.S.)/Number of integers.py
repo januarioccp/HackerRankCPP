d48ec442225f8a60a5bc01c9d743b3ea395566b3
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-05-30 17:56:50 -0300

    Time limit excceded

--- Hack the Interview IV (U.S.)/Number of integers.py
+++ Hack the Interview IV (U.S.)/Number of integers.py
@@ -21,11 +21,11 @@ def getNumberOfIntegers(L, R, K):
     resp = 0
     for number in range(int(L)+1,int(R)+1):
         if len(str(number)) - str(number).count('0') == K:
+            print(number)
             resp = resp + 1 
     return resp
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     L = input()
 
@@ -35,7 +35,5 @@ if __name__ == '__main__':
 
     ans = getNumberOfIntegers(L, R, K)
 
-    fptr.write(str(ans) + '\n')
-
-    fptr.close()
+    print(ans)
 
