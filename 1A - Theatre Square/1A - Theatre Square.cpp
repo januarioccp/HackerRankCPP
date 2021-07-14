2f43cee06d4f06538ef76d33402d7c5cfab19880
Tiago Januario <januarioccp@gmail.com>
2021-07-14 18:15:11 -0300

    Theatre

---
+++ 1A - Theatre Square/1A - Theatre Square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    string input;
+    cin>>input;
+    
+    for(int i = 0; i < input.size(); i++)
+    {
+        if()
+    }
+    unsigned long long x = ceil(n/a);
+    unsigned long long y = ceil(m/a);
+    cout<<x*y<<endl;
+    return 0;    
+}
